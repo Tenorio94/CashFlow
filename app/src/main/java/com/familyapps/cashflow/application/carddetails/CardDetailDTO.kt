ed917149aa765ae6ef1fa7817949f45b2b7166a5
Tenorio94 <geteca94@gmail.com>
2019-07-15 17:23:20 -0500

    Adding Card Number to Intent
    - Adds card number to send to the Card Details Activity from the Main Activity

--- app/src/main/java/com/familyapps/cashflow/application/carddetails/CardDetailDTO.kt
+++ app/src/main/java/com/familyapps/cashflow/application/carddetails/CardDetailDTO.kt
@@ -4,8 +4,14 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class CardDetailDTO(val mainCardName : String, val mainCardStatement : String, val mainCardImage: Int) : Parcelable{
-    var cardName : String = mainCardName
-    var cardStatement : String = mainCardStatement
-    var cardImage : Int = mainCardImage
+class CardDetailDTO(
+    val mainCardName: String,
+    val mainCardStatement: String,
+    val mainCardImage: Int,
+    val mainCardNumber: String
+) : Parcelable {
+    var cardName: String = mainCardName
+    var cardStatement: String = mainCardStatement
+    var cardNumber: String = mainCardNumber
+    var cardImage: Int = mainCardImage
 }

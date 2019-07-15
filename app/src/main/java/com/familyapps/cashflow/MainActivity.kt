cc6462b1edbf57987255b428ef96ee65dc660f17
Tenorio94 <geteca94@gmail.com>
2019-07-15 17:30:15 -0500

    Main workflow refactored.
    - Refactors Database Extensions and adds Statement Functionality for dynamic mode.
    - Refactors Main Activity.

--- app/src/main/java/com/familyapps/cashflow/MainActivity.kt
+++ app/src/main/java/com/familyapps/cashflow/MainActivity.kt
@@ -5,10 +5,16 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.util.Log
+import android.view.View
 import com.familyapps.cashflow.application.maincardsummary.CardSummaryAdapter
 import com.familyapps.cashflow.application.maincardsummary.CardSummaryStatement
-import com.familyapps.cashflow.infraestructure.databaseextensions.populateBanks
+import com.familyapps.cashflow.infraestructure.databaseextensions.*
 import com.familyapps.cashflow.model.CashFlowDatabase
+import com.familyapps.cashflow.model.DbWorkerThread
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,38 +23,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var summaryRecViewAdapter: CardSummaryAdapter
 
     private var cardSummaryList: ArrayList<CardSummaryStatement> = arrayListOf()
-    private var cashFlowDb: CashFlowDatabase? = CashFlowDatabase.getInstance(this)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        createCardList()
-        createRecycleView()
-        populateBanks(cashFlowDb)
+    companion object {
+        var currentMonth = LocalDate.now().month.getDisplayName(TextStyle.FULL, Locale.US)
+        var email = "gtenoriocastillo@gmail.com"
     }
 
-    private fun createCardList() {
-        cardSummaryList.add(
-            CardSummaryStatement(
-                R.drawable.ic_android,
-                "$4,440.00",
-                "American Express Platinum"
-            )
-        )
-        cardSummaryList.add(
-            CardSummaryStatement(
-                R.drawable.ic_event_seat,
-                "$4,780.00",
-                cardName = "Banamex Oro"
-            )
-        )
-        cardSummaryList.add(
-            CardSummaryStatement(
-                R.drawable.ic_flight_land,
-                "$130.00",
-                cardName = "Volaris 2.0"
-            )
-        )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val LOG_TAG = "CF_MA_MainCreate"
+        val cashFlowDb: CashFlowDatabase? = CashFlowDatabase.getInstance(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(LOG_TAG, String.format("Id of resource: %s",R.drawable.ic_android.toString()))
+        populateBanks(cashFlowDb)
+        populateCards(cashFlowDb)
+        cardSummaryList = populateStatements(cashFlowDb)
+        createRecycleView()
+
+        cardSummaryList.forEach {
+            Log.i(LOG_TAG, String.format("%s statement for %s card.", it.cardSummaryStatement, it.cardSummaryName))
+        }
     }
 
     private fun createRecycleView() {
@@ -62,10 +56,10 @@ class MainActivity : AppCompatActivity() {
         summaryRecyclerView.adapter = summaryRecViewAdapter
     }
 
-    fun addCard() {
+    fun addCard(view: View) {
         cardSummaryList.add(
             CardSummaryStatement(
-                R.drawable.ic_flight_land, "$6,780.00", "Banncomer Azul"
+                R.drawable.ic_flight_land, "$6,780.00", "Banncomer Azul", "4"
             )
         )
         Log.i("CreateRV", cardSummaryList[cardSummaryList.size - 1].cardSummaryName)
--- app/src/main/java/com/familyapps/cashflow/infraestructure/databaseextensions/DatabaseExtensions.kt
+++ app/src/main/java/com/familyapps/cashflow/infraestructure/databaseextensions/DatabaseExtensions.kt
@@ -1,15 +1,232 @@
 package com.familyapps.cashflow.infraestructure.databaseextensions
 
 import android.util.Log
+import com.familyapps.cashflow.MainActivity
+import com.familyapps.cashflow.MainActivity.Companion.currentMonth
+import com.familyapps.cashflow.R
+import com.familyapps.cashflow.application.maincardsummary.CardSummaryStatement
+import com.familyapps.cashflow.infraestructure.convertDoubleToCash
 import com.familyapps.cashflow.model.CashFlowDatabase
+import com.familyapps.cashflow.model.DbWorkerThread
 import com.familyapps.cashflow.model.bank.Bank
+import com.familyapps.cashflow.model.card.CreditCard
+import com.familyapps.cashflow.model.statement.Statement
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.time.Instant
+import kotlin.collections.ArrayList
+import kotlin.random.Random
+
+private lateinit var workerThread: DbWorkerThread
+
+fun populateStatements(cashFlowDb: CashFlowDatabase?): ArrayList<CardSummaryStatement> {
+    workerThread = DbWorkerThread("cashFlowThread")
+    workerThread.start()
+
+    val currentEmail = MainActivity.email
+    val cardStatementList = arrayListOf<CardSummaryStatement>()
+
+    val LOG_TAG = "CF_MA_StatementPopulating"
+    Log.i(LOG_TAG, "Running statement population...")
+
+    GlobalScope.launch {
+        val statementRepository = cashFlowDb?.statementRepository()
+        val cardRepository = cashFlowDb?.cardRepository()
+        val statementList = statementRepository?.findStatementsByMonthAndEmail(currentMonth, currentEmail)
+        var cardSearch: CreditCard
 
 
+        if (statementList!!.isNotEmpty()) {
+            statementList.forEach {
+                Log.i(LOG_TAG, String.format("Statement for Card Number %s found... Looking for card", it.cardNumber))
+                cardSearch = cardRepository?.findCreditCardByCardNumber(it.cardNumber)!!
 
-fun populateBanks(cashFlowDb : CashFlowDatabase?) {
-    val LOG_TAG = "MA_BankPopulating"
+                val cardStatement =
+                    CardSummaryStatement(mapCardWithImage(cardSearch.bankCardName), convertDoubleToCash(it.statementAmount), cardSearch.cardName, it.cardNumber)
+                cardStatementList.add(cardStatement)
+            }
+        } else {
+            statementRepository.insertBatchStatement(createStatementList())
+        }
+    }
+
+    Thread.sleep(50)
+    return cardStatementList
+}
+
+
+fun mapCardWithImage(cardName: String) : Int {
+
+    return when (cardName) {
+        "AMEX_PT" -> R.drawable.amex_platinum_card
+        "BMX_CLASICA" -> R.drawable.banamex_clasica
+        "BMX_ORO" -> R.drawable.banamex_oro
+        "INV_VOLARIS2" -> R.drawable.invex_volaris2
+        else -> R.drawable.ic_android
+    }
+}
+
+fun createStatementList(): List<Statement> {
+    val amexStatement = Statement(
+        null,
+        currentMonth,
+        Random.nextDouble(15000.00),
+        Instant.now(),
+        Instant.now().plusSeconds(2419200),
+        "371775436701005",
+        "tenorio94",
+        "gtenoriocastillo@gmail.com"
+    )
+
+    val banamexOroStatement = Statement(
+        null,
+        currentMonth,
+        Random.nextDouble(15000.00),
+        Instant.now(),
+        Instant.now().plusSeconds(2419200),
+        "5499490538837717",
+        "79260547",
+        "gtenoriocastillo@gmail.com"
+    )
+
+    val banamexClasicaStatement = Statement(
+        null,
+        currentMonth,
+        Random.nextDouble(15000.00),
+        Instant.now(),
+        Instant.now().plusSeconds(2419200),
+        "5288439112077716",
+        "79260547",
+        "gtenoriocastillo@gmail.com"
+    )
+
+    val invexStatement = Statement(
+        null,
+        currentMonth,
+        Random.nextDouble(15000.00),
+        Instant.now(),
+        Instant.now().plusSeconds(2419200),
+        "4196910100284245",
+        "gtenoriocastillo@gmail.com",
+        "gtenoriocastillo@gmail.com"
+    )
+
+    return listOf(amexStatement, banamexOroStatement, banamexClasicaStatement, invexStatement)
+}
+
+fun createCardList(): List<CreditCard> {
+    val amexCard = CreditCard(
+        null,
+        "American Express Platinum",
+        "AMEX_PT",
+        "371775436701005",
+        Instant.now().plusSeconds(155520000),
+        Instant.now(),
+        "CREDIT",
+        "American Express",
+        "tenorio94",
+        "gtenoriocastillo@gmail.com",
+        Instant.now().plusNanos(216000),
+        120000.00,
+        Random.nextDouble(120000.00),
+        3000.00,
+        330.00
+    )
+    val banamexOro = CreditCard(
+        null,
+        "Banamex Oro",
+        "BMX_ORO",
+        "5499490538837717",
+        Instant.now().plusSeconds(155520000),
+        Instant.now(),
+        "CREDIT",
+        "Banamex",
+        "79260547",
+        "gtenoriocastillo@gmail.com",
+        Instant.now().plusNanos(216000),
+        111000.00,
+        Random.nextDouble(111000.00),
+        980.00,
+        110.00
+    )
+    val banamexClasica = CreditCard(
+        null,
+        "Banamex Clasica",
+        "BMX_CLASICA",
+        "5288439112077716",
+        Instant.now().plusSeconds(155520000),
+        Instant.now(),
+        "CREDIT",
+        "Banamex",
+        "79260547",
+        "gtenoriocastillo@gmail.com",
+        Instant.now().plusNanos(216000),
+        19000.00,
+        Random.nextDouble(19000.00),
+        680.00,
+        80.00
+    )
+    val invexVolaris = CreditCard(
+        null,
+        "INVEX Volaris 2.0",
+        "INV_VOLARIS2",
+        "4196910100284245",
+        Instant.now().plusSeconds(155520000),
+        Instant.now(),
+        "CREDIT",
+        "Invex",
+        "gtenoriocastillo@gmail.com",
+        "gtenoriocastillo@gmail.com",
+        Instant.now().plusNanos(216000),
+        35000.00,
+        Random.nextDouble(35000.00),
+        3600.00,
+        380.00
+    )
+    return listOf(amexCard, banamexClasica, banamexOro, invexVolaris)
+}
+
+fun createBankList(): ArrayList<Bank>{
+    val bankList = arrayListOf<Bank>()
+    val banks = listOf<String>(
+        "American Express",
+        "BBVA",
+        "Banamex",
+        "Invex",
+        "Inbursa",
+        "Santander",
+        "Scotiabank"
+    )
+    banks.forEach {
+        bankList.add(Bank(null, it))
+    }
+    return bankList
+}
+
+fun populateCards(cashFlowDb: CashFlowDatabase?) {
+    val cardRepository = cashFlowDb?.cardRepository()
+    var cardList: List<CreditCard> = listOf()
+
+    val LOG_TAG = "CF_MA_CardPopulating"
+    Log.i(LOG_TAG, "Running card population...")
+
+    GlobalScope.launch {
+        cardList = cardRepository?.findAllCreditCardsByEmail(MainActivity.email)!!
+
+        if (cardList.isNotEmpty()) {
+            Log.i(LOG_TAG, "Cards have already been populated...")
+            cardList.forEach {
+                Log.i(LOG_TAG, String.format("%s card found with number %s", it.cardName, it.cardNumber))
+            }
+        } else {
+            cardRepository?.insertBatchCreditCard(createCardList())
+            Log.i(LOG_TAG, "Cards empty... DB Updated")
+        }
+    }
+}
+
+fun populateBanks(cashFlowDb: CashFlowDatabase?) {
+    val LOG_TAG = "CF_MA_BankPopulating"
     Log.i(LOG_TAG, "Running bank population...")
 
     GlobalScope.launch {
@@ -22,20 +239,7 @@ fun populateBanks(cashFlowDb : CashFlowDatabase?) {
                 Log.i(LOG_TAG, String.format("%s in the list with id %d", it.bankName, it.id))
             }
         } else {
-            bankList = arrayListOf<Bank>()
-            val banks = listOf<String>(
-                "American Express",
-                "BBVA",
-                "Banamex",
-                "Invex",
-                "Inbursa",
-                "Santander",
-                "Scotiabank"
-            )
-            banks.forEach {
-                bankList.add(Bank(null, it))
-            }
-            bankRepository?.insertBankBatch(bankList)
+            bankRepository?.insertBankBatch(createBankList())
             Log.i(LOG_TAG, "No Banks present... Banks have been inserted.")
         }
     }

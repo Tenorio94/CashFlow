9d6dcd6a3c4c6daaee6a0c6ee7bda2c3f6c9457d
Tenorio94 <geteca94@gmail.com>
2020-06-08 21:00:22 -0500

    Adding comments to important files.
    - Adds some documentation to the files that are most frequently used for easier debugging and coding,

--- app/src/main/java/com/familyapps/cashflow/infraestructure/DataConversion.kt
+++ app/src/main/java/com/familyapps/cashflow/infraestructure/DataConversion.kt
@@ -8,12 +8,27 @@ import java.time.ZoneId.systemDefault
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
-
+/**
+ * Function used to convert a number into a String, this contains the traditional format of when we
+ * manage currencies. This is used to display the information on the UI
+ *
+ * @param doubleToConvert - Double containing the number fetched from the database of a transaction.
+ *
+ * @return String - Double formatted into a String in format $X,XXX,XXX.XX
+ */
 fun convertDoubleToCash(doubleToConvert: Double) : String{
     val currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US)
     return currencyFormatter.format(doubleToConvert)
 }
 
+/**
+ * Function used to convert an Instant date into a readable String, this is used in WW format which
+ * includes the DD-MM-YYYY format.
+ *
+ * @param instantToConvert - Instant fetched from the database to convert.
+ *
+ * @return String - Converted Date as in format DD-MM-YYYY
+ */
 fun convertInstantToString(instantToConvert: Instant) : String {
     val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
         .withLocale(Locale.ENGLISH)
--- app/src/main/java/com/familyapps/cashflow/infraestructure/SessionManagement.kt
+++ app/src/main/java/com/familyapps/cashflow/infraestructure/SessionManagement.kt
@@ -9,6 +9,11 @@ import java.time.Instant
 
 private val logTag = "CF_SM_CREDENTIALS"
 
+/**
+ * Function used to set a new session into the application. Based on the session we can skip the
+ * login or prompt the user to enter his credentials. Credentials include: username, email, and
+ * sessionDate, which expires.
+ */
 fun setSession(applicationContext: Context, emailTxt: String, passwordTxt: String, creationDate: Instant) {
     val userSession = PreferenceManager.getDefaultSharedPreferences(applicationContext)
     val credentials: SharedPreferences.Editor = userSession.edit()
@@ -18,6 +23,10 @@ fun setSession(applicationContext: Context, emailTxt: String, passwordTxt: Strin
     credentials.apply()
 }
 
+/**
+ * Function used to verify there is a current session. In case the session exceeds 90 days of
+ * activity it will be ended and prompt the user to enter new credentials.
+ */
 fun getSession(applicationContext: Context, creationDate: Instant): String {
     val userSession = PreferenceManager.getDefaultSharedPreferences(applicationContext)
     val createdSession = Instant.ofEpochSecond(userSession.getLong("sessionDate", -1))
@@ -40,6 +49,11 @@ fun getSession(applicationContext: Context, creationDate: Instant): String {
     return userName!!
 }
 
+/**
+ * Function triggered when the logout button is clicked. This causes the credentials to be flushed
+ * and prompt the user to enter a new sessions once the application is re-opened or continues
+ * the workflow.
+ */
 fun deleteSession(applicationContext: Context) {
     val userSession = PreferenceManager.getDefaultSharedPreferences(applicationContext)
     val credentials: SharedPreferences.Editor = userSession.edit()
